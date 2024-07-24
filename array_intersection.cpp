6b5a83737618c6b649db9bc033fb687ce3b9560e
preetikumari5 <147963preetikumari@gmail.com>
2024-07-24 20:49:20 +0530

    code

--- array_intersection.cpp
+++ array_intersection.cpp
@@ -3,38 +3,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// brute force approach
-
-
-
 // optimal approach
-vector < int > FindInterSection(int arr1[], int arr2[], int n, int m) 
-{
-    int i=0,j=0;
-    vector <int> InterSection;
-    while(i<n && j<m)
-    {
-        if (arr1[i] == arr2[j]) // Case 1 and 2
-        {
-            if (InterSection.size() == 0 || InterSection.back() != arr1[i])
-                InterSection.push_back(arr1[i]);
-                i++;
-        } else // case 3
-        {
-            if (InterSection.size() == 0 || InterSection.back() != arr2[j])
-                InterSection.push_back(arr2[j]);
-                j++;
-        }
+
+  vector<int> intersectionOfArrays(vector<int>A, vector<int>B){
+    
+  // Declare ans array.
+  vector <int> ans;
+
+  int i = 0, j = 0; 
+
+  // to traverse the arrays
+  while (i < A.size() && j < B.size()) {
+      
+    //if current element in i is smaller
+    if (A[i] < B[j]) { 
+      i++;
+    } else if (B[j] < A[i]) {
+      j++;
+    } else {
+        
+      //both elements are equal
+      ans.push_back(A[i]); 
+      i++;
+      j++;
     }
+  }
+  
+  return ans;
+    
 }
 
 int main() {
-  int n = 10, m = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
-  vector < int > Union = FindInterSection(arr1, arr2, n, m);
-  cout << "InterSection of arr1 and arr2 is " << endl;
-  for (auto & val: Union)
-    cout << val << " ";
+    
+  // Array Initialisation.
+  vector < int > A {1,2,3,3,4,5,6,7};
+  vector < int > B {3,3,4,4,5,8};
+
+  vector<int> ans = intersectionOfArrays(A,B);
+  
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
+
   return 0;
 }
