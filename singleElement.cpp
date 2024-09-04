9c1e481fb4d9d41e42f855ff6e15edcbc4bc4171
preetikumari5 <147963preetikumari@gmail.com>
2024-09-04 23:10:42 +0530

    single element

--- singleElement.cpp
+++ singleElement.cpp
@@ -12,3 +12,31 @@ public:
         return ans;
     }
 };
+
+
+// 2nd approach
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int high=nums.size()-1;
+        if(nums.size()==1)
+        {
+            return nums[0];
+        } 
+        for(int i=0;i<nums.size()-1;i=i+2)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                return nums[i];
+            }
+            if(nums[high]!=nums[high-1])
+            {
+                return nums[high];
+            }
+            high=high-2;
+            
+        } 
+        return 1;   
+    }
+};
