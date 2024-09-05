7c55a757e00c201d14bcb84321002ffd4f8f48da
preetikumari5 <147963preetikumari@gmail.com>
2024-09-05 10:32:22 +0530

    Search element in matrix

---
+++ Search_Matrix.cpp
@@ -0,0 +1,20 @@
+// leetcode 74 search in an matrix
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
