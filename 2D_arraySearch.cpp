f6d3bb93418b5ab2e7251fb24cb4fa1dd3070e48
preetikumari5 <147963preetikumari@gmail.com>
2024-09-15 23:08:55 +0530

    search in 2D array

--- 2D_arraySearch.cpp
+++ 2D_arraySearch.cpp
@@ -1,7 +1,23 @@
 // leetcode 240
 
+// solution 1
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0 , j= n-1;
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
 
-
+// solution 2
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
