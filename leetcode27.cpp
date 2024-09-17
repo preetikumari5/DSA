19619d02389872173e3c565233efe0512d892ec8
preetikumari5 <147963preetikumari@gmail.com>
2024-09-18 00:05:40 +0530

    remove elemnt from array

---
+++ leetcode27.cpp
@@ -0,0 +1,17 @@
+// remove element from array
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[l] = nums[i];
+                l++;
+            }
+        }
+        return l;
+    }
+};
