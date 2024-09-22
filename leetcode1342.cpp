5ea4f4a1505b00dc2be8f9e4b945768fc83188ca
preetikumari5 <147963preetikumari@gmail.com>
2024-09-22 12:05:07 +0530

    count number of steps to reach 0

---
+++ leetcode1342.cpp
@@ -0,0 +1,23 @@
+// count number of steps
+
+class Solution {
+public:
+    
+    int numberOfSteps(int num) {
+        int count = 0;
+        while(num>0)
+        {
+            if(num%2 == 0) 
+            {
+                num = num/2;
+                count++;
+            }
+            else
+            {
+                num = num-1;
+                count++;
+            }
+        }
+        return count;
+    }
+};
