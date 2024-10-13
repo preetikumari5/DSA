045554a0d254217ba3f645233585e84a04cf3f97
preetikumari5 <147963preetikumari@gmail.com>
2024-10-13 17:33:30 +0530

    binary palindrome

---
+++ binary_palindrome.java
@@ -0,0 +1,36 @@
+// binary palindrome
+
+import java.util.*;
+class Main {
+    public static boolean isPalindrome(int x)
+    {
+        String binaryString = Integer.toBinaryString(x);
+        
+        int left = 0 , right = binaryString.length() -1;
+        while(left<right)
+        {
+            if(binaryString.charAt(left)!=binaryString.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean f = isPalindrome(n);
+        if(f)
+        {
+            System.out.println("it has binary palindrome representation");
+        }
+        else
+        {
+            System.out.println("it does not have binary palindrome representation");
+        }
+    }
+}
