16559eb96b0d0cd9114d0fbbd24ecefc3fdde026
preetikumari5 <147963preetikumari@gmail.com>
2024-10-13 18:35:19 +0530

    1's sequence

---
+++ 1_sequence.cpp
@@ -0,0 +1,33 @@
+Longest Sequence of 1 after flipping a bit
+
+import java.util.*;
+public class Main {
+    public static int flipBit(int n)
+    {
+        String binary = Integer.toBinaryString(n);
+        int currentLength = 0;
+        int previousLength = 0;
+        int maxLength = 0;
+        for(char bit : binary.toCharArray()) 
+        {
+            if(bit=='1')
+            {
+                currentLength++;
+            }
+            else
+            {
+                maxLength = Math.max(previousLength+currentLength+1 , maxLength);
+                previousLength = currentLength;
+                currentLength = 0;
+            }
+        }
+        maxLength = Math.max(previousLength+currentLength+1 , maxLength);
+        return maxLength;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(flipBit(n));
+    }
+}
