286e78b3b6dc4aea890b98ee5312af0da91ca50b
preetikumari5 <147963preetikumari@gmail.com>
2024-10-13 18:42:17 +0530

    code

---
+++ SwapNibbles.java
@@ -0,0 +1,21 @@
+// swapping nibbles
+
+public import java.util.*;
+public class Main {
+    public static int swap(int x)
+    {
+        int left = (x & 0xF0) >> 4;
+        int right = (x & 0x0F) << 4;
+        return (right | left);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = swap(num);
+        System.out.println(result);
+        
+    }
+} {
+    
+}
