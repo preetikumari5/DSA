ca25d6cf5a137b47303e099bbfd765a42af4bbd5
preetikumari5 <147963preetikumari@gmail.com>
2024-10-26 11:51:47 +0530

    subsequence using recursion

---
+++ subsequence_Rec.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void subseq(int index , vector<int> &ds , int arr[],int n)
+{
+    if(index==n)
+    {
+        for(auto it:ds){
+            cout <<it << " ";
+        }
+        if(ds.size()==0)
+        {
+            cout<<"{}";
+        }
+        cout<<endl;
+        return;
+    }
+
+    // take the particular index into subsequence
+    ds.push_back(arr[index]);
+    subseq(index+1,ds,arr,n);
+
+    // not take index element in the subsequence
+    ds.pop_back();
+    subseq(index+1,ds,arr,n);
+}
+
+int main()
+{
+    int arr[] = {3,1,2};
+    int n = 3;
+    vector<int> ds;
+    subseq(0,ds,arr,n);
+    return 0;
+}
