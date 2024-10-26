94d361f0e7ed68701f02b9c7e9ff9796f5a74233
preetikumari5 <147963preetikumari@gmail.com>
2024-10-26 11:05:16 +0530

    code

---
+++ leetcode125.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.length()-1;
+        if(s.length()<=1)
+        {
+            return true;
+        }
+        while(start<end)
+        {
+            while (start<end && !isalnum(s[start]))
+            {
+                start++;
+            }
+           while (start<end && !isalnum(s[end]))
+            {
+                end--;
+            }
+            if((tolower(s[start]) != tolower(s[end]))) return false;
+            start++;
+            end--;
+            
+        }
+        return true;
+    }
+};
