4916f1ddf2b795321caa4999e3f6bda60b93f093
preetikumari5 <147963preetikumari@gmail.com>
2024-11-27 19:08:25 +0530

    combination

---
+++ combination.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main 
+{
+    public static int fact(int n)
+    {
+        int f = 1;
+        if(n==1 || n==0) return 1;
+        for(int i=1;i<=n;i++)
+        {
+            f = f*i;
+        }
+        return f;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int r = sc.nextInt();
+        int result = fact(n) / (fact(n-r)*fact(r));
+        System.out.println(result);
+    }
+}
