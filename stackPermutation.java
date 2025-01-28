fc355b185472a7aa9b87c599bf9eefb6264b48b9
preetikumari5 <147963preetikumari@gmail.com>
2025-01-28 21:32:44 +0530

    check maximum element in a queue using sliding window

--- stackPermutation.java
+++ stackPermutation.java
@@ -1,6 +1,5 @@
 // stack permutation
 import java.util.*;
-
 class Main {
     static Boolean check(int ip[], int op[], int n) {
         Stack<Integer> s = new Stack<>();
@@ -14,25 +13,19 @@ class Main {
                 j++;
             }
         }
-
         return s.isEmpty();
     }
-
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int input[] = new int[n];
         int output[] = new int[n];
-
         for (int i = 0; i < n; i++) {
             input[i] = sc.nextInt();
         }
-
         for (int i = 0; i < n; i++) {
             output[i] = sc.nextInt();
         }
-
         if (check(input, output, n)) {
             System.out.println("Yes");
         } else {
