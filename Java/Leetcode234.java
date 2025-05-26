ea294d134c7219b1d94e397ee2f58f265b24cf72
preetikumari5 <147963preetikumari@gmail.com>
2025-05-26 12:04:16 +0530

    palindromic Linked list

---
+++ Java/Leetcode234.java
@@ -0,0 +1,44 @@
+// check LL is palindrome or not
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Leetcode234 {
+    public ListNode reverseList(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode newNode = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newNode;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = reverseList(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+        while(second!=null){
+            if(first.val != second.val){
+                reverseList(newHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;   
+        }
+        reverseList(newHead);
+        return true;
+    }
+}
