30806d065eb9a3bf7a9a4f636eda7a0bc158e4eb
preetikumari5 <147963preetikumari@gmail.com>
2025-08-01 09:08:58 +0530

    balanced binary tree

---
+++ Java/Leetcode110.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int check(TreeNode root){
+        if(root==null) return 0;
+
+        int lh = check(root.left);
+        int rh = check(root.right);
+
+        if(lh==-1 || rh==-1) return -1;
+        if(Math.abs(lh-rh)>1) return -1;
+
+        return 1 + Math.max(lh,rh);
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        if(check(root)==-1) return false;
+        return true; 
+    }
+}
