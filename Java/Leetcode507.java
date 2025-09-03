1fdd51f225c1aaa6716d88e244790cd8f026b3c0
preetikumari5 <147963preetikumari@gmail.com>
2025-09-04 00:18:20 +0530

    base 7

---
+++ Java/Leetcode507.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if(num==0) return "0";
+        StringBuilder str = new StringBuilder();
+        boolean neg = num < 0;
+        num = Math.abs(num);
+        while(num>0){
+            str.append(num%7);
+            num=num/7;
+        }
+        if(neg) str.append("-");
+        return str.reverse().toString();
+    }
+}
